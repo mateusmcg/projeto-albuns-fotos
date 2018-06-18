45697222017ac175061563871ee3b110f0b577db
Mateus Cerqueira <mateus.mcg@gmail.com>
2018-06-18 14:28:23 -0300

    Fianlizando script de build com Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,31 @@ module.exports = function (grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'src/app.js',
+                src: 'app.js',
                 dest: 'build/app.min.js'
             }
-        }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    { expand: true, src: ['app.js', 'index.html'], dest: 'build/', filter: 'isFile' },
+                    { expand: true, src: ['assets/**'], dest: 'build/' },
+                    { expand: true, flatten: true, src: ['node_modules/bootstrap/dist/fonts/**'], dest: 'build/assets/fonts' },
+                    { expand: true, flatten: true, src: ['node_modules/bootstrap/dist/css/bootstrap.min.css'], dest: 'build/assets/css', filter: 'isFile' },
+                    { expand: true, flatten: true, src: ['node_modules/bootstrap/dist/css/bootstrap.min.css.map'], dest: 'build/assets/css', filter: 'isFile' },
+                    { expand: true, flatten: true, src: ['node_modules/bootstrap/dist/js/bootstrap.min.js'], dest: 'build/assets/js', filter: 'isFile' },
+                    { expand: true, flatten: true, src: ['node_modules/jquery/dist/jquery.min.js'], dest: 'build/assets/js', filter: 'isFile' }
+                ],
+            },
+        },
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['copy']);
 
 };
--- app.js
+++
@@ -1,6 +0,0 @@
-var teste = 213;
-var abc = 2314;
-
-array.forEach(element => {
-    teste++;
-});
