e61c9fb6189e67a5bc623f16309fddad269a162b
Mateus Cerqueira Gonçalves <888388@sga.pucminas.br>
2018-06-26 22:16:25 -0300

    Fazendo chamada na API do spotify para recuperar albums

--- app/app.js
+++ app/app.js
@@ -3,10 +3,30 @@ var myApp = (function () {
     var init = function () {
         if ($auth.validateAuth()) {
             console.debug('Autenticado.', localStorage.getItem('access_token'));
+            initEvents();
         } else {
             $auth.authenticate();
         }
     };
+
+    function initEvents() {
+        $("#btnPesquisar").click(function (e) {
+            e.preventDefault();
+            var query = $("#txtPesquisar").val();
+            return $services.getAlbums(query).done(function (result) {
+                console.debug(result);
+                var divAlbumList = $("#albumList");
+                var albums = result.albums.items;
+                $.map(albums, function (album, index) {
+                    var albumImage = album.images[2];
+                    var divAlbum = $('<div class="album"><img src="' + albumImage.url + '" height="' + albumImage.height + '" width="' + albumImage.width + '" />' + album.name + '</div>')
+                    divAlbumList.append(divAlbum);
+                });
+            }).fail(function (error) {
+                console.error(error);
+            });
+        });
+    }
 
     return {
         init: init
--- app/services.js
+++ app/services.js
@@ -1,7 +1,17 @@
 var $services = (function () {
 
     var getAlbums = function (query) {
-
+        return $.ajax({
+            url: "https://api.spotify.com/v1/search",
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("access_token")
+            },
+            data: {
+                q: query,
+                type: 'album'
+            }
+        });
     };
 
     var getMusics = function (albumId) {
